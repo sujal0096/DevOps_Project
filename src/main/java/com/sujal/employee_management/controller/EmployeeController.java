5141772f645c07741fc546e9bc000de32072ec3e
Sujal <sujal@gmail.com>
2026-07-29 22:58:33 +0530

    Updated home page to Version 5

--- src/main/java/com/sujal/employee_management/controller/EmployeeController.java
+++ src/main/java/com/sujal/employee_management/controller/EmployeeController.java
@@ -21,7 +21,7 @@ public class EmployeeController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to Employee Management - Version 4";
+        return "Welcome to Employee Management - Version 5";
     }
 
     // Get all employees
