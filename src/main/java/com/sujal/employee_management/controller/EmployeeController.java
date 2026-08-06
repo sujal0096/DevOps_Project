cb3161dc9da4df9aee4c45622b7c076277fda159
Sujal <sujal@gmail.com>
2026-08-07 00:21:50 +0530

    version 10 to homepage

--- src/main/java/com/sujal/employee_management/controller/EmployeeController.java
+++ src/main/java/com/sujal/employee_management/controller/EmployeeController.java
@@ -21,7 +21,7 @@ public class EmployeeController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to Employee Management - Version 9";
+        return "Welcome to Employee Management - Version 10";
     }
 
     // Get all employees
